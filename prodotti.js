2a8e64bc1194ca86add6c527958581c5b2f11c14
Maral996 <alessiomarches@hotmail.it>
2026-02-09 21:09:42 +0100

    Create prodotti.js

---
+++ prodotti.js
@@ -0,0 +1,18 @@
+// Database centrale dei prodotti
+const LISTA_PRODOTTI = { 
+    1: { 
+        nome: "Spazzole Tergicristallo", 
+        prezzo: 30, 
+        immagine: " " 
+    }, 
+    2: { 
+        nome: "Batteria 60Ah", 
+        prezzo: 75, 
+        immagine: " " 
+    }, 
+    3: { 
+        nome: "Lampadina Bosch H7", 
+        prezzo: 12, 
+        immagine: "img/lampadinah7bosch.jpg" // Senza immagine: non apparirà nulla nel box
+    }
+};
